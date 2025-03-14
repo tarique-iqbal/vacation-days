d50b73afc4f344e99f676725a29394a35f8dbf28
Tarique Iqbal <live.iqbal@gmail.com>
2025-03-14 16:07:58 +0100

    Change text

--- src/VacationDays/Template/vacation_days.php
+++ src/VacationDays/Template/vacation_days.php
@@ -2,7 +2,7 @@
 
 foreach ($employees as $employee) {
     if (0.0 === $employee->getVacationDaysOfGivenYear()) {
-        echo $employee->getName() . ': Inapplicable' . PHP_EOL;
+        echo $employee->getName() . ': Not applicable' . PHP_EOL;
     } else {
         echo $employee->getName() . ': ' . $employee->getVacationDaysOfGivenYear() . PHP_EOL;
     }
